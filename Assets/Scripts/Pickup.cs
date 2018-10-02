a46520d832e45a0fb537e3e170a660921f50e0d4
Alexandra Lu <cml694@nyu.edu>
2018-10-02 01:13:27 -0400

    initial commit

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pickup : MonoBehaviour
+{
+	private RaycastHit hit;
+	private LayerMask mask;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		mask = LayerMask.GetMask("Items");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (Physics.Raycast(transform.position, transform.forward,
+				out hit, Mathf.Infinity, mask.value))
+			{
+				Destroy((hit.transform.gameObject));
+				//this.transform.parent = transform;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+	public float timer = 60f;
+
+	public Text timeRemaining; 
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > 0)
+		{
+			timer -= Time.deltaTime;
+			timeRemaining.text = "Timer: " + (int)timer;
+		}
+		else
+		{
+			Time.timeScale = 0;
+		}
+	}
+}
