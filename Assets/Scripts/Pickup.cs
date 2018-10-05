19fcf1be66f9835d71343632fc651dae89570f37
Alexandra Lu <cml694@nyu.edu>
2018-10-05 00:03:00 -0400

    And put them down...in the suitcase!

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+
+//place on  main camera
+//shoot a ray from the camera/player to the item, then pick up/parent it when clicked
+
 public class Pickup : MonoBehaviour
 {
 	private RaycastHit hit;
-	private LayerMask mask;
+	private LayerMask itemMask;
+	private LayerMask suitcaseMask;
 	private GameObject holding;
 
 	public Transform AttachMe;
+	public Transform BoopOverTheSuitcase;
 	
 	// Use this for initialization
 	void Start ()
 	{
-		mask = LayerMask.GetMask("Items");
+		itemMask = LayerMask.GetMask("Items");
+		suitcaseMask = LayerMask.GetMask("Suitcase");
 	}
 	
 	void Update ()
@@ -26,18 +33,34 @@ public class Pickup : MonoBehaviour
 		float maxRayDistance = 3f;
 
 		
-		if (Input.GetMouseButtonDown(0)&& holding ==null)		
+		if (Input.GetMouseButtonDown(0))		
 		{
-			
-			if (Physics.Raycast(myRay, out hit, maxRayDistance, mask.value))
+
+			if (holding == null)
 			{
-				Debug.Log("munch");
-				holding = hit.transform.gameObject;
-				holding.transform.SetParent(transform); //this but  not
+				if (Physics.Raycast(myRay, out hit, maxRayDistance, itemMask.value))
+				{
+					Debug.Log("pick up object");
+					holding = hit.transform.gameObject;
+					holding.transform.SetParent(transform); //this but  not
+				}
+			}
+
+			else
+			{
+				if (Physics.Raycast(myRay, maxRayDistance, suitcaseMask.value))
+				{
+					Debug.Log("put down object");
+					holding.transform.SetParent(null);
+					holding.transform.position = BoopOverTheSuitcase.position;
+					holding.AddComponent<Rigidbody>();
+					holding = null;
+
+				}
 			}
 		}
 
-		if (holding != null)
+		if (holding != null)   //so if holding
 		{
 			holding.transform.position = Vector3.Lerp(holding.transform.position, AttachMe.position, .1f);
 		}
