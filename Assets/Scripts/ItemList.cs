7b0e98c3cda2c85d39b82d880717cb85137f09fb
Alexandra Lu <cml694@nyu.edu>
2018-10-11 00:30:37 -0400

    Suitcase collider and some colors and perfume !

---
+++ Assets/Scripts/ItemList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemList : MonoBehaviour
+{
+
+	public Image strikethrough;
+	private bool collected = false;
+
+	private void Start()
+	{
+		strikethrough.rectTransform.localScale = new Vector3(0, strikethrough.rectTransform.localScale.y, 1);
+	}
+
+	void Update () {
+		if (!collected)
+		{
+			strikethrough.rectTransform.localScale = Vector3.Lerp(strikethrough.rectTransform.localScale, new Vector3(1, .04f, 1), Time.deltaTime);
+			strikethrough.rectTransform.anchoredPosition = new Vector3(8 + (strikethrough.rectTransform.localScale.x * strikethrough.rectTransform.rect.width * .5f), strikethrough.rectTransform.anchoredPosition.y, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reticle : MonoBehaviour {
+
+	//place on main camera
+	//shoot a ray to items and suitcase to change color so the player knows when they are in range to interact with something
+	
+	private LayerMask itemMask;
+	private LayerMask suitcaseMask;
+
+	public Image reticle; 
+	
+	void Start ()
+	{
+		itemMask = LayerMask.GetMask("Items");
+		suitcaseMask = LayerMask.GetMask("Suitcase");
+	}
+	
+	
+	
+	void Update () {
+		//define
+		Ray myRay = new Ray(transform.position, transform.forward);
+		//set max distance
+		float maxRayDistance = 3f;
+		
+		//shooty shooty shoot
+		//if ray collides with item, turn red
+		if (Physics.Raycast(myRay, maxRayDistance, itemMask.value))
+		{
+
+			reticle.color = Color.red;
+		}
+
+		//if ray collides with suitcase, turn yellow
+		else if (Physics.Raycast(myRay, maxRayDistance, suitcaseMask.value))
+		{
+			reticle.color = Color.yellow;
+		}
+		
+		//if ray collides with nothing, stay white
+		else
+		{
+			reticle.color = Color.white;
+		}
+	}
+}
